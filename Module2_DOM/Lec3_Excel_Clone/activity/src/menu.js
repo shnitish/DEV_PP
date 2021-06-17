4e559511a499f9eec524288bf33ff18179ba49ce
nitish81299 <nitishkumar81299@gmail.com>
2021-06-17 15:55:57 +0530

    fixed some css styling

--- Module2_DOM/Lec3_Excel_Clone/activity/src/menu.js
+++ Module2_DOM/Lec3_Excel_Clone/activity/src/menu.js
@@ -1,40 +1,6 @@
-let menu = document.querySelector(".menu");
-let fileMenuOptions = document.querySelector(".file-menu-options");
-let homeMenuOptions = document.querySelector(".home-menu-options");
 let bold = document.querySelector(".bold");
 let underline = document.querySelector(".underline");
 let italic = document.querySelector(".italic");
-
-menu.addEventListener("click", function(e){
-    
-    if(e.target.classList.contains("menu"))
-    {
-        return;
-    }
-
-    let selectedMenu = e.target;
-    if(selectedMenu.classList.contains("active-menu"))
-    {
-        return;
-    }
-
-    document.querySelector(".active-menu").classList.remove("active-menu");
-    selectedMenu.classList.add("active-menu");
-
-
-    let menuName = selectedMenu.classList[0];
-    if(menuName == "home")
-    {
-        homeMenuOptions.classList.remove("hide");
-        fileMenuOptions.classList.add("hide");
-    }
-    
-    else
-    {
-        homeMenuOptions.classList.add("hide");
-        fileMenuOptions.classList.remove("hide");
-    }
-})
 
 bold.addEventListener("click", function(e){
     setFontStyle("bold", bold);  
